137d8466343d25eb999eca5063173cc0c03ebe76
Rakesh Kumar Sahani <nishadrakesh2054@gmail.com>
2025-11-18 14:28:20 +0545

    icons svg baat png maa rijan le gareko

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Button({ btnUrl, btnText, variant }) {
-  const arrowIcon = `${
-    process.env.PUBLIC_URL || ""
-  }/images/icons/arrow_white.svg`;
-
   return (
     <Link to={btnUrl} className={`cs_btn cs_style_1 ${variant}`}>
       <span>{btnText}</span>
       <i>
-        <img src={arrowIcon} alt="Icon" />
-        <img src={arrowIcon} alt="Icon" />
+        <img src="/images/icons/arrow_white.svg" alt="Icon" />
+        <img src="/images/icons/arrow_white.svg" alt="Icon" />
       </i>
     </Link>
   );
